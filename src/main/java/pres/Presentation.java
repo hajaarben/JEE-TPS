0d8fb59322cec8362f8a7d4e3286b6e112649a0c
Hajar <hajarbenkaireche2@gmail.com>
2024-03-19 13:28:44 +0000

    First Commit

---
+++ src/main/java/pres/Presentation.java
@@ -0,0 +1,19 @@
+package pres;
+
+import ext.DaoImpl2;
+import metier.IMetierImpl;
+
+public class Presentation {
+    public static void main(String[] args) {
+        /*
+        Injection des dépendances par
+        instanciation statique => new = couplage fort => la poubelle
+         */
+        DaoImpl2 dao=new DaoImpl2();
+        IMetierImpl metier=new IMetierImpl(dao);
+        //metier.setDao(dao);
+        System.out.println("Résultat="+metier.calcul());
+
+
+    }
+}
---
+++ src/test/java/metier/CalculTest.java
@@ -0,0 +1,13 @@
+package metier;
+
+import org.springframework.util.Assert;
+
+public class CalculTest {
+    private Calcul calcul;
+    public void testSomme(){
+        calcul=new Calcul();
+        double a=5; double b=9;
+        double expected=14;
+        double res=calcul.somme(a,b);
+    }
+}
