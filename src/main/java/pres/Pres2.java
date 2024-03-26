659546ccbbbbb27001fb6c3ee2e1d7fffd56fa41
Hajar <hajarbenkaireche2@gmail.com>
2024-03-26 11:37:13 +0000

    Second Commit

--- src/main/java/pres/Pres2.java
+++ src/main/java/pres/Pres2.java
@@ -4,6 +4,7 @@ import dao.IDao;
 import metier.IMetier;
 
 import java.io.File;
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
 import java.util.Scanner;
 
@@ -12,19 +13,20 @@ public class Pres2 {
         Scanner scanner=new Scanner(new File("config.txt"));
 
         String daoClassName=scanner.nextLine();
-        Class cDao= Class.forName(daoClassName);
+        Class<?> cDao= Class.forName(daoClassName);
         IDao dao=(IDao) cDao.newInstance();
 
-
         String metierClassName=scanner.nextLine();
-        Class cMetier= Class.forName(metierClassName);
-        IMetier metier=(IMetier) cMetier.newInstance();
+        Class<?> cMetier= Class.forName(metierClassName);
+        Constructor<?> constructor = cMetier.getConstructor(IDao.class);
+        IMetier metier=(IMetier) constructor.newInstance(dao);
 
         Method method=cMetier.getMethod("setDao",IDao.class);
         //metier.setDao(dao);
         method.invoke(metier,dao);
 
         System.out.println("Résultat=>"+metier.calcul());
-
+        scanner.close();
     }
 }
+
